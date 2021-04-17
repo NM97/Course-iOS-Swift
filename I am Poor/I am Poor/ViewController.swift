1869b553c37fdcc07937d76d088a7313723c5b67
oculorum <56758667+NM97@users.noreply.github.com>
2021-04-17 13:57:20 +0200

    I am poor - Swift
    I am poor - Swift

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Nataniel on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
